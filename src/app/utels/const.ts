ef02a405bd90f26899f20253dbf0f6f5ed2def42
abdalla <abdalla@54>
2024-07-18 02:06:45 +0300

    csdd

--- src/app/utels/const.ts
+++ src/app/utels/const.ts
@@ -1,3 +1,3 @@
 export const AERTICAL_PAGE=6;
-export const DOMAIN="https://nextjs-mkyy529j7-abdalla-elshahats-projects.vercel.app"
+export const DOMAIN="https://nextjs-theta-gray-50.vercel.app"
 // export const DOMAIN="http://localhost:3000"
